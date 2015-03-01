bff8e545fa448bef5be82034d7dd8f363dcaa6dd
Šimon Lukašík <slukasik@redhat.com>
2015-03-01 19:25:02 +0100

    Import puppet-foreman_scap_client from the environments

--- lib.sh
+++ lib.sh
@@ -233,6 +233,23 @@ function deploy_puppet_foreman_scap_client(){
 	popd
 }
 
+function json_path(){
+	local file=$1
+	local question="$2"
+	cat $file | python -c 'import json,sys; x = json.load(sys.stdin); print x'"$question"''
+}
+
+function import_puppet_foreman_scap_client(){
+	local server=$1
+	local json=`mktemp`
+	curl -k -u admin:admin -H 'Accept: version=2,application/json' https://$server/api/smart_proxies > $json
+	proxy_id=`json_path $json '["results"][0]["id"]'`
+	curl -k -u admin:admin -H "Accept: version=2,application/json" -H "Content-Type: application/json" -X POST \
+		https://foreman17test.local.lan/api/v2/smart_proxies/$proxy_id/import_puppetclasses > $json
+	json_path $json '["results"][0]["new_puppetclasses"]' | grep foreman_scap_client
+	json_path $json '["results"][1]["new_puppetclasses"]' | grep foreman_scap_client
+}
+
 function test_foreman_openscap(){
 	local server=$1
 	test_ensure_no_scap_content $server
